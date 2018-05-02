8ae581993bf6b254f044b3ed90dff5ad0f51488e
fionamiaojing <fionamiaojing@gmail.com>
2018-05-02 16:28:22 -0700

    wrote all method for Board.  Wrote findNrook

--- src/Board.js
+++ src/Board.js
@@ -79,12 +79,19 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
-      return false; // fixme
+      return this.rows()[rowIndex].reduce((a, b) => a + b) > 1;
     },
 
     // test if any rows on this board contain conflicts
     hasAnyRowConflicts: function() {
-      return false; // fixme
+      
+      for (var row = 0; row < this.rows().length; row++) {
+        if (this.hasRowConflictAt(row)) {
+          return true;
+        }
+      }
+      return false;
+      
     },
 
 
@@ -94,12 +101,21 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      var sumOfValues = 0;
+      for (var row = 0; row < this.rows().length; row++) {
+        sumOfValues += this.rows()[row][colIndex]; 
+      }
+      return sumOfValues > 1;    
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+      for (var col = 0; col < this.rows().length; col++) {
+        if (this.hasColConflictAt(col)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -109,12 +125,25 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var accumulator = 0;
+      for (var row = 0; row < this.rows().length; row++) {
+        if (this.rows()[row][majorDiagonalColumnIndexAtFirstRow] != undefined) {
+          accumulator += this.rows()[row][majorDiagonalColumnIndexAtFirstRow]; 
+        }
+        majorDiagonalColumnIndexAtFirstRow++;
+      };
+      return accumulator > 1;
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      var start = -(this.rows().length - 1);
+      for (var col = start; col < this.rows().length; col++) {
+        if (this.hasMajorDiagonalConflictAt(col)) {
+          return true;
+        }
+      }
+      return false;
     },
 
 
@@ -124,15 +153,31 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
+      var accumulator = 0;
+      for (var row = 0; row < this.rows().length; row++) {
+        if (this.rows()[row][minorDiagonalColumnIndexAtFirstRow] != undefined) {
+          accumulator += this.rows()[row][minorDiagonalColumnIndexAtFirstRow]; 
+        }
+        minorDiagonalColumnIndexAtFirstRow--;
+      };
+      return accumulator > 1;
+      
       return false; // fixme
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      var end = (this.rows().length * 2) - 1;
+      for (var col = 0; col < end; col++) {
+        if (this.hasMinorDiagonalConflictAt(col)) {
+          return true;
+        }
+      }
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
+                                                                    
 
 
   });
--- src/solvers.js
+++ src/solvers.js
@@ -16,10 +16,18 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
-
+  var solution = new Board({n: n});
+  for (var col = 0; col < n; col++) {
+    for (var row = 0; row < n; row++) {
+      solution.togglePiece(row, col);
+      if (solution.hasAnyRooksConflicts()) {
+        solution.togglePiece(row, col);
+      }
+    }
+  };
+  
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  return solution.rows();
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
